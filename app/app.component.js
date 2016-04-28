acc3a5b525a16473c984899585a4dfa67a269b79
Hassan Bazzi <contact@hassanbazzi.com>
2016-04-28 18:02:37 +1000

    holy crap these styles sucked

--- app/app.component.js
+++ app/app.component.js
@@ -11,7 +11,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var Hero, AppComponent;
+    var Hero, AppComponent, HEROES;
     return {
         setters:[
             function (core_1_1) {
@@ -31,17 +31,31 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                         id: 1,
                         name: 'Devourer'
                     };
+                    this.heroes = HEROES;
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n    <h1>{{title}}</h1>\n    <h2>{{hero.name}} details</h2>\n    <div>\n      <label>id:</label>\n       {{hero.id}}\n    </div>\n    <div>\n      <label>name: </label>\n      <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n    </div>"
+                        template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"#hero of heroes\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    <h2>{{hero.name}} details</h2>\n    <div>\n      <label>id:</label>\n       {{hero.id}}\n    </div>\n    <div>\n      <label>name: </label>\n      <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n    </div>",
+                        styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .heroes {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n  }\n  .heroes li {\n    cursor: pointer;\n    position: relative;\n    background-color: #524E50;\n    color: #eaeaea;\n    margin: 0 .5rem .5rem 0;\n    padding-right: .5em;\n    display: inline-block;\n    border-radius: 4px;\n    transition: all .3s;\n    border: 1px solid #232323;\n  }\n  .heroes li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .heroes li:hover {\n    background-color: #3C383A;\n    transform: scale(1.05);\n  }\n  .heroes .badge {\n    display: inline-block;\n    font-size: .8rem;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    padding: .5em;\n    margin-right: .4rem;\n    border-radius: 4px 0 0 4px;\n  }\n"]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             }());
             exports_1("AppComponent", AppComponent);
+            HEROES = [
+                { "id": 11, "name": "Devourer" },
+                { "id": 12, "name": "Panda" },
+                { "id": 13, "name": "Nomad" },
+                { "id": 14, "name": "Gauntlet" },
+                { "id": 15, "name": "Deadwood" },
+                { "id": 16, "name": "The Dark Lady" },
+                { "id": 17, "name": "Magmus" },
+                { "id": 18, "name": "Legionnaire" },
+                { "id": 19, "name": "Tempest" },
+                { "id": 20, "name": "Solstice" }
+            ];
         }
     }
 });
--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,12 @@ export class Hero {
     selector: 'my-app',
     template: `
     <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
     <h2>{{hero.name}} details</h2>
     <div>
       <label>id:</label>
@@ -17,7 +23,50 @@ export class Hero {
     <div>
       <label>name: </label>
       <input [(ngModel)]="hero.name" placeholder="name">
-    </div>`
+    </div>`,
+    styles:[`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    background-color: #524E50;
+    color: #eaeaea;
+    margin: 0 .5rem .5rem 0;
+    padding-right: .5em;
+    display: inline-block;
+    border-radius: 4px;
+    transition: all .3s;
+    border: 1px solid #232323;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    background-color: #3C383A;
+    transform: scale(1.05);
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: .8rem;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    padding: .5em;
+    margin-right: .4rem;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 
 export class AppComponent {
@@ -26,4 +75,17 @@ export class AppComponent {
     id: 1,
     name: 'Devourer'
   }
+  public heroes = HEROES;
 }
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Devourer" },
+  { "id": 12, "name": "Panda" },
+  { "id": 13, "name": "Nomad" },
+  { "id": 14, "name": "Gauntlet" },
+  { "id": 15, "name": "Deadwood" },
+  { "id": 16, "name": "The Dark Lady" },
+  { "id": 17, "name": "Magmus" },
+  { "id": 18, "name": "Legionnaire" },
+  { "id": 19, "name": "Tempest" },
+  { "id": 20, "name": "Solstice" }
+];
