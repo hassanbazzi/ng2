b6b461f3eb6f6711fa4bee6150776793e0c47a56
Hassan Bazzi <contact@hassanbazzi.com>
2016-04-29 07:32:06 +1000

    Hero details

--- app/app.component.js
+++ app/app.component.js
@@ -11,19 +11,13 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var Hero, AppComponent, HEROES;
+    var AppComponent, HEROES;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             }],
         execute: function() {
-            Hero = (function () {
-                function Hero() {
-                }
-                return Hero;
-            }());
-            exports_1("Hero", Hero);
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Heroes of Newerth';
@@ -33,7 +27,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n      <li\n      *ngFor=\"#hero of heroes\"\n      (click)=\"onSelect(hero)\"\n      [class.selected]=\"hero === selectedHero\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    <div *ngIf=\"selectedHero\">\n      <h2>{{selectedHero.name}} details</h2>\n      <div>\n        <label>id:</label>\n         {{selectedHero.id}}\n      </div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\">\n      </div>\n    </div>",
+                        template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n      <li\n      *ngFor=\"#hero of heroes\"\n      (click)=\"onSelect(hero)\"\n      [class.selected]=\"hero === selectedHero\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>",
                         styles: ["\n  .heroes {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n  }\n  .heroes li {\n    cursor: pointer;\n    position: relative;\n    background-color: #524E50;\n    color: #eaeaea;\n    margin: 0 .75rem .75rem 0;\n    padding-right: .5em;\n    display: inline-block;\n    border-radius: 4px;\n    transition: all .3s;\n    border: 1px solid #232323;\n  }\n  .heroes li.selected {\n    transform: scale(1.1);\n    background-color: #3C383A;\n  }\n  .heroes li:hover {\n    background-color: #3C383A;\n  }\n  .heroes .badge {\n    display: inline-block;\n    font-size: .8rem;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    padding: .5em;\n    margin-right: .4rem;\n    border-radius: 4px 0 0 4px;\n  }\n"]
                     }), 
                     __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,5 @@
 import {Component} from 'angular2/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import {Hero} from './hero';
 
 @Component({
     selector: 'my-app',
@@ -17,18 +13,7 @@ export class Hero {
       [class.selected]="hero === selectedHero">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
-    </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details</h2>
-      <div>
-        <label>id:</label>
-         {{selectedHero.id}}
-      </div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-    </div>`,
+    </ul>`,
     styles:[`
   .heroes {
     margin: 0 0 2em 0;
